1ddd4a8386fdb6e34db07483da51e0d17c39eb4f
Germán hv <ghove7@gmail.com>
2023-11-22 18:41:21 -0600

    Create Travel.kt

---
+++ Travel.kt
@@ -0,0 +1,36 @@
+package clases
+
+import java.time.temporal.TemporalAmount
+
+abstract class Travel() {
+    abstract protected val country: String
+    abstract protected val city: String
+    protected val serviceType = "Viaje"
+    protected var reserved = false
+    protected var paidAmount = 0
+
+
+    abstract fun quotePrice(numDays: Int)
+
+    protected abstract fun getPrice(numDays: Int):Int
+
+    fun reserve(numDays: Int){
+        if(reserved){
+            println("""¡Ya reservaste ti viaje!",
+                    "Pais: $country",
+                    "Ciudad: $city",
+                    "Precio: $paidAmount".trimMargin()""")
+            return
+        }
+    val amount = getPrice(numDays)
+    if(amount == 0){
+        return
+    }
+    reserved = true
+    paidAmount = amount
+    println("""¡Viaje reservado exitosamente!"
+            "Pais: $country",
+            "Ciudad: $city",
+            "Precio: $paidAmount""")
+    }
+}
